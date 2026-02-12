23f59673bf282dc68d2720c994fd41b3c1b834bd
Josh Fox (Loco) <joshfox1987@gmail.com>
2026-02-12 11:01:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3066782500-b50dd",
+  "appId": "1:642303940569:web:f107ba74578549d7fb2bc8",
+  "apiKey": "AIzaSyAiBRa5IvitB3QGNJDZw5vzsPphPN5L0tQ",
+  "authDomain": "studio-3066782500-b50dd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "642303940569"
+};
